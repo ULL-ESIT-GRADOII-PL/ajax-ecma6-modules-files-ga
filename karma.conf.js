58899c1f27303179d63fe5fc44d75ef50b8e3b1e
Gabriel Melian Hernandez <alu0100819786@ull.edu.es>
2016-04-14 18:26:21 +0100

    Añadida

--- karma.conf.js
+++ karma.conf.js
@@ -1,1 +1,45 @@
+module.exports = function(config) {
+  config.set({
 
+    basePath: '',
+
+    frameworks: ['mocha', 'chai'],
+    
+    files: [
+      'public/csv.js',
+      'test/test.js',
+      'test/mocha.js',
+      'test/mocha.css',
+    ],
+    
+    client: {
+          mocha: {
+            ui: 'bdd'
+          }
+    },
+    
+    exclude: [
+      'gulpfile.js'
+    ],
+    
+    preprocessors: {
+      'test/index.html': ['html2js']
+    },
+    
+    reporters: ['progress'],
+
+  port: 9876,
+
+  colors: true,
+
+  logLevel: config.LOG_INFO,
+
+  autoWatch: true,
+
+     browsers: ['Firefox', 'PhantomJS'],
+
+  singleRun: false
+
+
+    });
+};
