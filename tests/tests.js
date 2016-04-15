8b5fe27f6595fb1e172df89ad39e623b503db9b4
Gabriel Melian Hernandez <alu0100819786@ull.edu.es>
2016-04-15 09:05:43 +0100

    Añadidas mas pruebas para #3

--- tests/tests.js
+++ tests/tests.js
@@ -3,9 +3,9 @@ var expect = chai.expect;
 describe("CSV", function () {
     describe("Funcion Calculate", function() {
     
-      it("Debería aceptar una entrada simple", function() {  
+      it("Deberia aceptar una entrada simple", function() {  
    
-          var cadena = "1,2,3,4,a";
+          var cadena = '"1","2","3","4","a"';
           
           var r = calculate(cadena);
           
@@ -14,7 +14,30 @@ describe("CSV", function () {
           expect(r[0].value[2]).to.equal('3');
           expect(r[0].value[3]).to.equal('4');
           expect(r[0].value[4]).to.equal('a');
+      });
+     
+      it('Debería aceptar un número con coma', function() {
+          var cadena = '"3,4"';
+          var r = calculate(cadena);
           
+          expect(r[0].value[0]).to.equal('3,4');
+      });
+      
+      
+      it("Debería aceptar una cadena con comas a los lados", function() {
+          var cadena = '",CSV,"';
+          var r = calculate(cadena);
+      
+          expect(r[0].value[0]).to.equal(',CSV,');
+      });
+      
+      
+      it("Deberia aceptar cadenas separadas por comas y espacios", function() {
+          var cadena = ' ,"CSV"';
+          var r = calculate(cadena);
+      
+          expect(r[0].value[0]).to.equal(' ');
+          expect(r[0].value[1]).to.equal('CSV');
       });
    });
 });
