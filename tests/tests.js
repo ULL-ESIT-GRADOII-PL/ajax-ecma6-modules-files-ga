8839f3dc3a88b1b5780d48ce9e37e1c07e58ea2e
Alejandro Marrero Díaz <alexmd54@gmail.com>
2016-04-15 08:19:34 +0000

    Modificadas las funciones para adaptarlas a la nueva sintaxis de ecmasrcript 6

--- tests/tests.js
+++ tests/tests.js
@@ -1,13 +1,13 @@
 var expect = chai.expect;
 
 describe("CSV", function () {
-    describe("Funcion Calculate", function() {
+    describe("Funcion Calculate", () => {
     
-      it("Deberia aceptar una entrada simple", function() {  
+      it("Deberia aceptar una entrada simple", () => {  
    
-          var cadena = '"1","2","3","4","a"';
+          let cadena = '"1","2","3","4","a"';
           
-          var r = calculate(cadena);
+          let r = calculate(cadena);
           
           expect(r[0].value[0]).to.equal('1');
           expect(r[0].value[1]).to.equal('2');
@@ -16,25 +16,25 @@ describe("CSV", function () {
           expect(r[0].value[4]).to.equal('a');
       });
      
-      it('Debería aceptar un número con coma', function() {
-          var cadena = '"3,4"';
-          var r = calculate(cadena);
+      it('Debería aceptar un número con coma', () => {
+          let cadena = '"3,4"';
+          let r = calculate(cadena);
           
           expect(r[0].value[0]).to.equal('3,4');
       });
       
       
-      it("Debería aceptar una cadena con comas a los lados", function() {
-          var cadena = '",CSV,"';
-          var r = calculate(cadena);
+      it("Debería aceptar una cadena con comas a los lados", () => {
+          let cadena = '",CSV,"';
+          let r = calculate(cadena);
       
           expect(r[0].value[0]).to.equal(',CSV,');
       });
       
       
-      it("Deberia aceptar cadenas separadas por comas y espacios", function() {
-          var cadena = ' ,"CSV"';
-          var r = calculate(cadena);
+      it("Deberia aceptar cadenas separadas por comas y espacios", () => {
+          let cadena = ' ,"CSV"';
+          let r = calculate(cadena);
       
           expect(r[0].value[0]).to.equal(' ');
           expect(r[0].value[1]).to.equal('CSV');
